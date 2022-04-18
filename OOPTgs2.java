721ad9bc71bf11097bf89967f8e498768a493381
RubenMaaf <103195656+RubenMaaf@users.noreply.github.com>
2022-04-18 21:31:25 +0700

    Setor Tugas 2

---
+++ OOPTgs2.java
@@ -0,0 +1,14 @@
+package oop.tgs.pkg2;
+public class OOPTgs2 {
+    public static void main(String[] args) {
+        hitungvbalok volbalok= new hitungvbalok();
+        
+        volbalok.SetPanjang(5);
+        volbalok.SetLebar(8);
+        volbalok.SetTinggi(9);
+        volbalok.CetakHasil();
+        
+        
+    }
+}
+    
